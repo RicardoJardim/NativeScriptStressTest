5b8b6a49fa697ed580916892bcd62b1c953a8eb8
Ricardo <ricardo.l.jardim@hotmail.com>
2021-04-07 23:59:27 +0100

    stage 2.7

--- testapp/app/main-page.ts
+++ testapp/app/main-page.ts
@@ -4,19 +4,178 @@ a code-behind file. The code-behind is a great place to place your view
 logic, and to set up your page’s data binding.
 */
 
-import { EventData, Page } from "@nativescript/core";
+import {
+	Http,
+	HttpResponse,
+	EventData,
+	Page,
+	Button,
+	AbsoluteLayout,
+	knownFolders,
+	Folder,
+	File,
+} from "@nativescript/core";
+import * as geolocation from "@nativescript/geolocation";
+import * as imagepicker from "@nativescript/imagepicker";
+import * as camera from "@nativescript/camera";
+
 import { MainPageModel } from "./main-view-model";
 
+const documents: Folder = <Folder>knownFolders.documents();
+const folder: Folder = <Folder>documents.getFolder("");
+
+var page;
 // Event handler for Page 'navigatingTo' event attached in main-page.xml
 export function navigatingTo(args: EventData) {
-	const page = <Page>args.object;
+	page = <Page>args.object;
 	var context = new MainPageModel();
 	page.bindingContext = context;
 	/* 	view.className = "active";
 	 */
-	context.buttons = [
+	/* context.buttons = [
 		page.getViewById("0"),
 		page.getViewById("1"),
 		page.getViewById("2"),
-	];
+	]; */
+}
+
+export function addViews(args: EventData) {
+	let elm = page.getViewById("addElements");
+
+	console.log(new Date());
+	for (let index = 1; index <= 1000; index++) {
+		let btn = new AbsoluteLayout();
+		btn.backgroundColor = "white";
+		btn.width = 220;
+		btn.height = 230;
+		elm.addChild(btn);
+	}
+	console.log(new Date());
+}
+
+export function addButtons(args: EventData) {
+	let elm = page.getViewById("addElements");
+
+	console.log(new Date());
+	for (let index = 1; index <= 1000; index++) {
+		let btn = new Button();
+		btn.text = "My New Label";
+		btn.backgroundColor = "yellow";
+		btn.width = 230;
+		btn.height = 240;
+		elm.addChild(btn);
+	}
+	console.log(new Date());
+}
+
+export function MakeHttp() {
+	console.log(new Date());
+
+	Http.request({
+		url: "https://random-data-api.com/api/users/random_user?size=100",
+		method: "GET",
+	}).then(
+		(response: HttpResponse) => {
+			// Argument (response) is HttpResponse
+			console.log(`Response Status Code: ${response.statusCode}`);
+			writeFile("users", `${response.content}`);
+		},
+		(e) => {
+			alert(e.toString());
+		}
+	);
+	Http.request({
+		url: "https://random-data-api.com/api/bank/random_bank?size=100",
+		method: "GET",
+	}).then(
+		(response: HttpResponse) => {
+			// Argument (response) is HttpResponse
+			console.log(`Response Status Code: ${response.statusCode}`);
+			writeFile("banks", `${response.content}`);
+		},
+		(e) => {
+			alert(e.toString());
+		}
+	);
+}
+
+export function gallery(): void {
+	const context = imagepicker.create({
+		mode: "single",
+	});
+
+	// Request permissions, show the images list and process the selection
+	context
+		.authorize()
+		.then(() => {
+			return context.present();
+		})
+		.then((selection) => {
+			console.log(selection);
+		})
+		.catch((e) => {
+			// process error
+			console.log(e);
+		});
+}
+
+export function openCamera(): void {
+	camera.requestPermissions().then(
+		function success() {
+			camera
+				.takePicture()
+				.then((imageAsset) => {
+					console.log(imageAsset);
+				})
+				.catch((err) => {
+					console.log("Error -> " + err.message);
+				});
+		},
+		function failure() {
+			alert("Failure");
+		}
+	);
+}
+
+export function gps(args: EventData): void {
+	//https://docs.nativescript.org/plugins/geolocation.html#usage
+	/* geolocation.isEnabled().then(
+		(value) => {
+			console.log(value);
+			if (value == false) {
+				geolocation.enableLocationRequest().then(
+					(value) => {
+						console.log(value);
+					},
+					(e) => {
+						console.log(e);
+					}
+				);
+			}
+		},
+		(e) => {
+			console.log(e);
+		}
+	); */
+}
+
+export function checkChanged(args: EventData) {
+	/* alert(args);
+	console.log(args); */
+}
+
+function writeFile(where: string, data: string): void {
+	var file: File = <File>folder.getFile(`${where}` + `.txt`);
+
+	file
+		.writeText(data)
+		.then(() => {
+			file.readText().then((res) => {
+				console.log(`Successfully saved in ${file.path}`);
+				console.log(new Date());
+			});
+		})
+		.catch((err) => {
+			console.log(err);
+		});
 }
--- testapp/app/main-view-model.ts
+++ testapp/app/main-view-model.ts
@@ -1,6 +1,46 @@
 import { Observable } from "@nativescript/core";
 
 export class MainPageModel extends Observable {
+	private _message: string;
+
+	constructor() {
+		super();
+		this.message = "Result = 0";
+	}
+
+	get message(): string {
+		return this._message;
+	}
+
+	set message(value: string) {
+		if (this._message !== value) {
+			this._message = value;
+			this.notifyPropertyChange("message", value);
+		}
+	}
+
+	onTap() {
+		this.updateMessage();
+	}
+
+	private updateMessage() {
+		console.log(new Date());
+		let result = 0;
+		for (let j = 1; j <= 5; j++) {
+			for (let k = 1; k <= 100000; k++) {
+				result +=
+					Math.log2(k) + (3 * k) / (2 * j) + Math.sqrt(k) + Math.pow(k, j - 1);
+			}
+		}
+		console.log(new Date());
+		this.message = "Result = " + result;
+	}
+}
+
+/* 
+import { Observable } from "@nativescript/core";
+
+export class MainPageModel extends Observable {
 	private _counter: number;
 	private _message: string;
 	private _tab: number;
@@ -12,7 +52,7 @@ export class MainPageModel extends Observable {
 		// Initialize default values.
 		this._counter = 42;
 		this.updateMessage();
-		this._tab = 0;
+		this._tab = 1;
 	}
 
 	get message(): string {
@@ -32,7 +72,8 @@ export class MainPageModel extends Observable {
 	}
 
 	onChangingTab(args) {
-		if (this._tab != parseInt(args.object.id)) {
+		alert(args.object.id);
+		 if (this._tab != parseInt(args.object.id)) {
 			this._tab = parseInt(args.object.id);
 			this.buttons.forEach((el) => {
 				el.getChildAt(0).tintColor = "black";
@@ -40,7 +81,7 @@ export class MainPageModel extends Observable {
 			});
 			args.object.getChildAt(0).tintColor = "white";
 			args.object.getChildAt(1).color = "white";
-		}
+		} 
 	}
 
 	private updateMessage() {
@@ -52,3 +93,5 @@ export class MainPageModel extends Observable {
 		}
 	}
 }
+
+*/
